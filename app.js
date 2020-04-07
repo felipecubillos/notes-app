65e243a2c1f4c6fdf30d2271f4e03a01544e011f
Felipe Cubillos <felipe.cubillos@outlook.com>
2020-04-06 21:18:21 -0500

    configuring notes app options to use add, real, remove, list using yargs

---
+++ app.js
@@ -0,0 +1,82 @@
+const yargs = require('yargs');
+
+yargs.version("1.3");
+
+// add notes
+yargs
+    .command(
+        'add <title> <body>',
+        'adding a note',
+        (yargs) => {
+            yargs.positional('title', {
+                describe: 'You should provide a note title',
+                type: 'string'
+            }),
+            yargs.positional('body',
+            {describe: 'you should provide a note content',
+             type: 'string'    
+        })
+        },
+        function (yargs) {
+            console.log('Note added : ' + yargs.title);
+            console.log(' the body of the note is : '+ yargs.body);
+        }
+    );
+
+// remove notes
+yargs
+    .command(
+        'remove <title>',
+        'remove a note ',
+        function (yargs) {
+            yargs.positional(
+                'title',
+                {
+                    describe: 'provide a name for a note to remove',
+                    type: 'string'
+                }
+            )
+        },
+        function () {
+            console.log(' note deleted: ' + yargs.title)
+        }
+    );
+
+// read notes
+yargs
+    .command(
+        'read <title>',
+        'read a note ',
+        (yargs) => {
+            yargs.positional(
+                'title',
+                {
+                    describe: 'Title note to read',
+                    type: 'string'
+                }
+            )
+        },
+        function (yargs) {
+            console.log(' title readed : ' + yargs.title)
+        }
+    );
+
+// list notes
+yargs
+    .command(
+        'list <title>',
+        'list a note ',
+        (yargs) => {
+            yargs.positional('title',
+                {
+                    describe: 'a title to list ',
+                    type: 'string'
+                }
+            )
+        },
+        function (yargs) {
+            console.log(' note listed: ' + yargs.title)
+        }
+    );
+yargs.parse();
+//console.log(yargs.argv);
